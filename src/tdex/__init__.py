b1075ef289c41d1c3b91ba65034da880a8c45688
acbuie <aidancbuie@gmail.com>
2025-09-16 13:55:17 -0600

    feat: Basic cyclopts app

--- src/tdex/__init__.py
+++ src/tdex/__init__.py
@@ -1,2 +1,15 @@
+"""`tdex`, the pokedex for your terminal."""
+
+from cyclopts import App
+
+app = App()
+
+
+@app.default
 def main() -> None:
+    """CLI entrypoint."""
     print("Hello from tdex!")
+
+
+if __name__ == "__main__":
+    app()
